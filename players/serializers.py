11f7a03964cb1b311ea690c5233918bdf9e60240
chwillchwill <chwillchwill@gmail.com>
2023-02-15 21:46:59 -0500

    Nuking initial setup
    Removing initial setup to replace

--- players/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Player
-
-class PlayerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Player
-        fields = ('PlayerId', 'Username', 'Email', 'ProfilePicture', 'Playstyle', 'CompositeSkillLevel', 'Attitude')
