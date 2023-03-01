071a0f97638c1dd1c5ac9aeaf5526bb7221de89c
Jeffrey Ganulin <jganulin6@gmail.com>
2023-03-01 13:37:34 -0500

    * Extract Registration E2E test to its own spec. * Add Cypress rules to eslint config to fix false syntax errors

--- frontend/cypress/e2e/login-page.spec.cy.js
+++ frontend/cypress/e2e/login-page.spec.cy.js
@@ -8,40 +8,4 @@ describe('Login Page', () => {
     cy.get('.roundedBlue').click();
     cy.contains('User Profile');
    });
-  
-   it('registers new users', () => {
-    // --------- LOGIN PAGE ---------
-    // Visit the React Local Server that is running
-    cy.visit('localhost:3000'); 
-    // Click Register Button
-    cy.get(':nth-child(5) > .Link').click()
-    
-    // ------- REGISTRATION PAGE ---------
-    // Check if user is on Registration page (string value)
-    cy.contains('Registration')
-    // Fill in the input fields and validate that they are filled correctly
-    cy.get('#fName').type('Some').should('have.value', 'Some');
-    cy.get('#lName').type('Tester').should('have.value', 'Tester');
-    cy.get('#email').type('test@test.com').should('have.value', 'test@test.com');
-    cy.get('#password').type('p@ssw0rd!').should('have.value', 'p@ssw0rd!');
-    cy.get('#confirmPassword').type('p@ssw0rd!').should('have.value', 'p@ssw0rd!');
-    cy.get('#playstyle').select('Semi');
-    cy.get('#submit').click(); // Click the submit button
-
-    // --------- CONTINUE REGISTRATION PAGE ---------
-    // Check if user is on Registration Confirmation page (string value)
-    cy.contains('Continue Registration');
-    // Test User Interactions on the Page
-    cy.get('#username').type('TestUsername23').should('have.value', 'TestUsername23');
-    cy.get('[for="avatar3"] > img').click();
-    cy.get('.roundedBlue').click() // Click to button continue
-
-    // --------- REGISTRATION COMPLETE PAGE ---------
-    cy.contains('Registration Complete');
-    cy.get('.alignSelfCenter').click(); // Click to button continue
-
-    // --------- LOGIN PAGE ---------
-    // Check to ensure user is back on Login page
-    cy.contains('Login');
-   });
 });
