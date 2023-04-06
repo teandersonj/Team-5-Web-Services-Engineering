50c0f95bfe7b4b6e919f8dd4a4e60fe6154df2fc
Jeffrey Ganulin <jganulin6@gmail.com>
2023-04-06 15:43:11 -0400

    Simplify Register using new backend logic Use token for PUT /player/id via ContinueReg

--- frontend/src/routes/Auth/ContinueRegistration.jsx
+++ frontend/src/routes/Auth/ContinueRegistration.jsx
@@ -62,13 +62,6 @@ export default function ContinueRegistration(props) {
         }
     };
 
-    // Style the avatar images
-    // const imageStyle = {
-    //     display: "block",
-    //     width: "100%",
-    //     height: "100%"
-    // };
-
     const handlePlaystyleChange = (event) => {
         setFormState((prev) => ({ ...prev, playstyle: event.target.value }));
     };
@@ -106,7 +99,7 @@ export default function ContinueRegistration(props) {
                 "Content-Type": "application/json",
                 
                 // TODO: Make PUT /api/player/:id guarded with token?
-                // "Authorization": `Bearer ${user.accessToken}`
+                "Authorization": `Bearer ${user.accessToken}`
             },
             data: {
                 "pk": user.id,
