93cbb976a236a4d4a7d08010998a785fa7d0303d
Jeffrey Ganulin <jganulin6@gmail.com>
2023-04-25 20:36:01 -0400

    Extend access token expiry to 5 hours

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,7 +105,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
